e3dcf77f89be366854a93e273a92233a2f65a33c
syedmujeeb <syedmujeeb9151@gmail.com>
2025-04-14 15:19:05 +0530

    Authorisation with securityContextHolder

--- hmsapp/src/main/java/com/hmsapp/config/JwtFilter.java
+++ hmsapp/src/main/java/com/hmsapp/config/JwtFilter.java
@@ -1,20 +1,29 @@
 package com.hmsapp.config;
 
+import com.hmsapp.entity.User;
+import com.hmsapp.repository.UserRepository;
 import com.hmsapp.service.JWTService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Optional;
+
 @Component
 public class JwtFilter extends OncePerRequestFilter {
     private JWTService jwtService;
+    private UserRepository userRepository;
 
-    public JwtFilter(JWTService jwtService){
+    public JwtFilter(JWTService jwtService, UserRepository userRepository1){
         this.jwtService = jwtService;
+        this.userRepository = userRepository1;
     }
 
     @Override
@@ -26,8 +35,17 @@ public class JwtFilter extends OncePerRequestFilter {
         if(token!=null && token.startsWith("Bearer ")) {
             String tokenVal = token.substring(7);
             String username = jwtService.getUsername(tokenVal);
-            System.out.println(username);
+            Optional<User> opUser = userRepository.findByUsername(username);
+            if(opUser.isPresent()){
+                User user = opUser.get();
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(user, null, null);
+                authenticationToken.setDetails(new WebAuthenticationDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            }
         }
+        filterChain.doFilter(request, response);
 
     }
 }
--- hmsapp/src/main/java/com/hmsapp/config/SecurityConfig.java
+++ hmsapp/src/main/java/com/hmsapp/config/SecurityConfig.java
@@ -4,12 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter){
+        this.jwtFilter = jwtFilter;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf().disable().cors().disable();
+//        http.addFilterBefore(jwtFilter, AuthorizationFilter.class);// this line filters only the url's which comes with the token
 
         http.authorizeHttpRequests().anyRequest().permitAll();
         return http.build();
